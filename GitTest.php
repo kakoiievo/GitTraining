8d89c163bbbcc0e12860c48cdb7ea0356b751518
leohsu <leohsu@kkbox.com>
2018-10-26 09:22:56 +0800

    update html to php code

--- GitTest.php
+++ GitTest.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Test Git</title>
-</head>
-<body>
-	
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	XDXDXDXD
-	
-</body>
-</html>
+<?php
+
+echo "this is php code\n";
